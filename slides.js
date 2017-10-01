f9f823a409806f58b3bbe2dd4855c587ee3041f7
Ethan Brown <e@zepln.com>
2017-10-01 10:07:44 -0700

    Initial commit.

---
+++ slides.js
@@ -0,0 +1,6 @@
+module.exports = [
+  { id: '45123041-9a4c-4035-9e69-4e82b3e4a926', name: 'Q1' },
+  { id: 'c8cfe426-8495-4e40-bc31-dd29991a4ccc', name: 'Q2' },
+  { id: '830acec0-c84a-4af1-8134-79be1b7d6469', name: 'Q3' },
+  { id: '3edd6c5c-4949-4154-bfe3-4fe94e45332f', name: 'Q4' },
+]
